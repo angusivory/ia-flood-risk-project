05a6e1c3b1cdacbc71615c0558a28a129072b7aa
Adhvika Muthukrishnan <adhvika@Adhvikas-MacBook-Pro.local>
2024-02-26 16:52:03 +0000

    error fixed

--- Task2C.py
+++ Task2C.py
@@ -4,7 +4,7 @@
 
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
-
+from floodsystem.flood import stations_highest_rel_level
 
 def run():
     # Build list of stations
@@ -14,7 +14,7 @@ def run():
     update_water_levels(stations)
 #for loop that prints the names of the 10 stations at which the current relative level is highest, with the relative level beside each station name
     for i,j in stations_highest_rel_level(stations,10):
-        print(i,j)
+        print(i.name,j)
 
 if __name__ == "__main__":
     print("*** Task 2C: CUED Part IA Flood Warning System ***")
